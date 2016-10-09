63ee5a416eb7539cff6d3f41913c037a78780324
Maringanty Krishna Kavya <m.krishnakavya@gmail.com>
2016-10-08 19:18:56 -0500

    Add files via upload

---
+++ HomePage.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Spades Game</title>
+	
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	 <!--Linking the style sheet-->
+	<link rel="stylesheet" type="text/css" href="style/style.css">
+</head>
+<body>
+<!--
+Container with complete layout.
+The layout of the web page is divided into 3 rows and 3 columns. 
+-->
+<div class="container-fluid">
+<!--Row1-->
+<div class="row" style="height:35%; display: block;">
+<!--Row1-column 1 -->
+<div class="col-sm-4 col-md-4 col-lg-4">
+	
+</div>
+<!--Row1-column2-->
+<!--Player in the north-->
+<div class="col-sm-4 col-md-4 col-lg-4">
+	<h1>Player North</h1>
+</div>
+<!--Row1 Column3-->
+<div class="col-sm-4 col-md-4 col-lg-4">
+	
+</div>
+</div>
+<!--Row2-->
+<div class="row" style="height:35%; display: block;">
+<!--Row2-column 1 -->
+<!--Player in the west-->
+<div class="col-sm-4 col-md-4 col-lg-4">
+<h1>Player in the west</h1>
+</div>
+<!--Row2-column2-->
+<div class="col-sm-4 col-md-4 col-lg-4">
+	
+</div>
+<!--Row2 Column3-->
+<!--Player in the East-->
+<div class="col-sm-4 col-md-4 col-lg-4">
+<h1>Player in the east</h1>	
+</div>
+</div>
+<!--Row3-->
+<div class="row" style="height:30%; display: block;">
+<!--Row3-column 1 -->
+<div class="col-sm-4 col-md-4 col-lg-4">
+	
+</div>
+<!--Row3-column2-->
+<!--Player in the south/Human Player-->
+<div class="col-sm-4 col-md-4 col-lg-4">
+	<h1>Human Player</h1>
+</div>
+<!--Row3-column3-->
+<div class="col-sm-4 col-md-4 col-lg-4">
+	
+</div>
+</div>
+</div>
+</body>
+</html>
